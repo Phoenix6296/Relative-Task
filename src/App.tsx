6da71814ae668e20f2331636b1946d355e2fffac
Krishna <krishnabiswakarmasc@gmail.com>
2023-01-16 19:07:07 +0530

    Data fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import "./App.css";
 import Main from "./Components/Main/Main";
 const App = () => {
+  const headingIcon = require("./Assets/activity.png");
   return (
-    <div className="app">
+    <div className="app center">
+      <div className="app__title center">
+        <img src={headingIcon} alt="" />
+        <h3>Trending Assets</h3>
+      </div>
       <Main />
     </div>
   );
